be72540e112bd68c34759ed116dc4aaf290d76ff
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-08 00:45:18 +0100

    GET in react works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import { IWorkout } from "./interfaces";
+import axios from "axios";
+
+const backendUrl = 'http://localhost:3601';
+
 function App() {
+	const [workouts, setWorkouts] = useState<IWorkout[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}/workouts`);
+			const _workouts = response.data;
+			setWorkouts(_workouts);
+		})();
+	}, []);
 
 	return (
 		<>
 			<h1 className="text-2xl">Workout Site</h1>
-			<p>welcome to this site</p>
+			<p>There are {workouts.length} workouts.</p>
+			<ul>
+				{workouts.map(workout => {
+					return (
+						<li key={workout.id}>{workout.title}</li>
+					)
+				})}
+			</ul>
 		</>
 	);
 }
---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IWorkout {
+	id: string;
+	title: string;
+	duration: string;
+}
